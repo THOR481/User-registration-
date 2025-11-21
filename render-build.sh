4a2e1b971c31be9873740a9cd48b6405c9f849cd
THOR481 <shaikmeharaj481@gmail.com>
2025-11-21 21:33:12 +0530

    Fix migration issue

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+python manage.py migrate
