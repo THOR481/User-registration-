cfc8d9b713ac96af96e6e2cb5615120a3355e131
THOR481 <shaikmeharaj481@gmail.com>
2025-11-21 22:43:16 +0530

    Fix CSRF trusted origins

--- user_registration/settings.py
+++ user_registration/settings.py
@@ -39,7 +39,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.railway.app',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-f5136.up.railway.app',
+]
+
 
 
 # Application definition
